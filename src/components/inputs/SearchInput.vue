8dc7fcf3b4fca809d2e0a3baa6809103709c0ab3
IlyaSharafeev <ilyasharafeev01@gmail.com>
2022-12-21 15:33:59 +0200

    fix function debounce

--- src/components/inputs/SearchInput.vue
+++ src/components/inputs/SearchInput.vue
@@ -28,7 +28,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, defineProps } from "vue";
+import { onMounted, ref, defineProps, onDeactivated } from "vue";
 import { useSpotifyStore } from "@/store/spotify";
 
 defineProps({
@@ -38,6 +38,18 @@ defineProps({
 
 const searchInputValue = ref("");
 const spotifyStore = useSpotifyStore();
+
+function debounce(fn, delay = 0, immediate = false) {
+  let timeout;
+  return (...args) => {
+    if (immediate && !timeout) fn(...args);
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+}
 
 const checkInputState = () => {
   document.querySelector(".field").addEventListener("focus", function () {
@@ -76,39 +88,9 @@ onMounted(() => {
   checkInputState();
 });
 
-function debounce(callee, timeoutMs) {
-  // Как результат возвращаем другую функцию.
-  // Это нужно, чтобы мы могли не менять другие части кода,
-  // чуть позже мы увидим, как это помогает.
-  return function perform(...args) {
-    // В переменной previousCall мы будем хранить
-    // временную метку предыдущего вызова...
-    let previousCall = this.lastCall;
-
-    // ...а в переменной текущего вызова —
-    // временную метку нынешнего момента.
-    this.lastCall = Date.now();
-
-    // Нам это будет нужно, чтобы потом сравнить,
-    // когда была функция вызвана в этот раз и в предыдущий.
-    // Если разница между вызовами меньше, чем указанный интервал,
-    // то мы очищаем таймаут...
-    if (previousCall && this.lastCall - previousCall <= timeoutMs) {
-      clearTimeout(this.lastCallTimer);
-    }
-
-    // ...который отвечает за непосредственно вызов функции-аргумента.
-    // Обратите внимание, что мы передаём все аргументы ...args,
-    // который получаем в функции perform —
-    // это тоже нужно, чтобы нам не приходилось менять другие части кода.
-    this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs);
-
-    // Если таймаут был очищен, вызова не произойдёт
-    // если он очищен не был, то callee вызовется.
-    // Таким образом мы как бы «отодвигаем» вызов callee
-    // до тех пор, пока «снаружи всё не подуспокоится».
-  };
-}
+onDeactivated(() => {
+  clearSearchInput();
+});
 </script>
 
 <style lang="scss" scoped>
